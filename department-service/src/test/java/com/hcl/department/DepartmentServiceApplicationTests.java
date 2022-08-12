7ee120f4793c2dad5c233540395bbc7b8e9d320c
Paras Tomar <parastomar0109@gmail.com>
2022-08-12 13:44:46 +0530

    First Commit

---
+++ department-service/src/test/java/com/hcl/department/DepartmentServiceApplicationTests.java
@@ -0,0 +1,19 @@
+package com.hcl.department;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepartmentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+	@Test
+	void  doTest(){
+		
+		System.out.println("doTest");
+	}
+
+}
